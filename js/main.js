5ea0374a1f83e3aae42fc72924ea46413994a178
Alex <bigblackmushroom@gmail.com>
2018-12-31 23:06:43 +0100

    broken some features

--- js/main.js
+++ js/main.js
@@ -1,39 +1,30 @@
 $(document).ready(function(){
 
-    var i = 0
-    function typeout(message, target, speed){
-        if (i == 0){
-            document.getElementById(target).innerHTML += "<br>";
-        }
-        if (i < message.length){
-            document.getElementById(target).innerHTML += message.charAt(i);
-            i++;
-            setTimeout(function(){typeout(message, target, speed)}, speed);
-        }
-        else{
-            i=0;
-        }
-    }
     
-    $("input").on({
+    
+    $("#sample-textbox").on({
         "focus": function(){
-            document.getElementById("sample-textbox").style.boxShadow="0px 0px 5px 5px rgba(255,255,255,0.5)";
-            document.getElementById("sample-textbox").style.backgroundColor="rgba(255,255,255,0.5)";
-            document.getElementById("sample-textbox").style.border="none";
+            $("#sample-textbox").css({
+                "box-shadow": "0px 0px 5px 5px rgba(255,255,255,0.5)",
+                "background-color": "rgba(255,255,255,0.5)",
+                "border": "none"
+            });
         },
         "blur": function(){
-            document.getElementById("sample-textbox").style.boxShadow="none";
-            document.getElementById("sample-textbox").style.backgroundColor="rgba(255,255,255,0";
-            document.getElementById("sample-textbox").style.borderBottom="solid 1px black";
+            $("#sample-textbox").css({
+                "box-shadow": "none",
+                "background-color": "rgba(255,255,255,0)",
+                "border-bottom": "solid 1px black"
+            });
+
             $("input").slideUp(150);
             $("h1").slideUp(150);
-            typeout("I have been waiting for you, "+document.getElementById("sample-textbox").value+".", "typeout", "50");
+
+            typeoutJqueryBeta("I have been waiting for you, "+$("#sample-textbox").val()+".", "typeout", "5000");
+        },
+        "input": function(){
+            $("#output").text("Hello " + $("#sample-textbox").val());
         }
-
     })
 
-    $("input").on("input", function(){
-        document.getElementById("output").innerHTML = "Hello " + document.getElementById("sample-textbox").value;
-    })
-
-})
+}) 
