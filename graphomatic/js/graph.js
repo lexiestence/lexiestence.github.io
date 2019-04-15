33f8ba65e6de0fef83cef4fb59247b1f680bdf4f
Unknown <bigblackmushroom@gmail.com>
2019-04-15 03:59:02 +0200

    updated graphomatic

--- graphomatic/js/graph.js
+++ graphomatic/js/graph.js
@@ -1,14 +1,8 @@
 function drawGraph() {
     var canvas = document.getElementById("graph");
 
-    if (canvas.getContext) {
-
-        var width = canvas.width;
-        var height = canvas.height;
-
-        var ctx = canvas.getContext("2d");
-
-        ctx.clearRect (0, 0, width, height)
+    function drawScale() {
+        ctx.clearRect(0, 0, width, height)
 
         ctx.strokeStyle = "Black";
 
@@ -21,36 +15,54 @@ function drawGraph() {
         ctx.moveTo(width / 2, 0);
         ctx.lineTo(width / 2, height);
         ctx.stroke();
+    }
 
-
-        var x = -(width / 2);
-        var y;
-
-        var scaleX = document.getElementById("scaleX").value;
-        document.getElementById("outX").innerHTML = scaleX;
-        var scaleY = document.getElementById("scaleY").value;
-        document.getElementById("outY").innerHTML = scaleY;
-
+    function drawSine() {
         ctx.strokeStyle = "Red";
 
         ctx.beginPath();
         while (x <= width / 2) {
-            y = -Math.sin(x / scaleX) * scaleY;
+            y = (-Math.sin((x + panX) / scaleX) * scaleY)+panY;
             ctx.lineTo(x + width / 2, y + height / 2);
             x++
         }
         ctx.stroke();
+    }
 
+    function drawCosine(){
         ctx.strokeStyle = "Blue";
 
         x = -(width / 2);
 
         ctx.beginPath();
         while (x <= width / 2) {
-            y = -Math.cos(x / scaleX) * scaleY;
+            y = (-Math.cos((x + panX) / scaleX) * scaleY)+panY;
             ctx.lineTo(x + width / 2, y + height / 2);
             x++
         }
         ctx.stroke();
+    }
+
+    if (canvas.getContext) {
+        var width = canvas.width;
+        var height = canvas.height;
+
+        var x = -(width / 2);
+        var y;
+
+        var ctx = canvas.getContext("2d");
+
+        var scaleX = parseInt(document.getElementById("scaleX").value);
+        document.getElementById("outX").innerHTML = scaleX;
+        var scaleY = parseInt(document.getElementById("scaleY").value);
+        document.getElementById("outY").innerHTML = scaleY;
+        var panX = parseInt(document.getElementById("panX").value);
+        document.getElementById("outPanX").innerHTML = panX;
+        var panY = parseInt(document.getElementById("panY").value);
+        document.getElementById("outPanY").innerHTML = panY;
+
+        drawScale();
+        drawSine();
+        drawCosine();
     }
 }
