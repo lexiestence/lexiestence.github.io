8d0181a526cd7313264a08c4100dfd1417779717
Unknown <bigblackmushroom@gmail.com>
2019-04-20 17:41:21 +0200

    g2 regexes set up

--- graphomatic2/js/graph.js
+++ graphomatic2/js/graph.js
@@ -1,6 +1,12 @@
-var equationInitRegex = /^y=/;
-var parenthesisRegex = /\(.*\)/;
-var multiplicationRegex = /[0-9]x/;
+var equationInitRgx = /^y=/;
+var parenthesisRgx = /\(.*?\)/;
+var exponentRgx = /x\^[0-9]/;
+var multiplicateRgx = /([0-9]+\*?x|x\*[0-9]+)/;
+var divideXRgx = /x\\[0-9]+/;
+var divideByXRgx = /[0-9]+\\x/;
+var addRgx = /(x\+[0-9]+|[0-9]+\+x)/;
+var subtractXRgx = /[0-9]+-x/;
+var subtractFromXRgx = /x-[0-9]+/;
 
 var parenthesisArray = [];
 var multiplication = [];
@@ -11,20 +17,18 @@ function drawGraph(){
     var output = document.getElementById("output");
 
     try{
-        if(!equationInitRegex.exec(input)) throw "err: No init";
+        if(!equationInitRgx.exec(input)) throw "err: No init";
     }
     catch(e){
         displayError(e);
     }
 
-    input = input.replace(equationInitRegex, "");
+    input = input.replace(equationInitRgx, "");
     
-    while(parenthesisRegex.exec(input)){
-        parenthesisArray.push(parenthesisRegex.exec(input));
-        input = input.replace(parenthesisRegex, "");
+    while(parenthesisRgx.exec(input)){
+        parenthesisArray.push(parenthesisRgx.exec(input));
+        input = input.replace(parenthesisRgx, "");
     }
-
-    displayError(parenthesisArray);
 
     function displayError(e){
         output.innerHTML = e;
